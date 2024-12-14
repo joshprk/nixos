bb89c882509264ce809b19f7198a0ab8b56fc883
Joshua Park <joshuprk@gmail.com>
2024-12-14 02:03:11 -0500

    add tailscale key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,4 +2,5 @@ let
   ssh = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHm7m7TJ5xzqsD4cC2ACFwCBE5LBQdn0RG1+ov4E3JTY joshua@PC";
 in {
   "user.age".publicKeys = [ssh];
+  "tailscale.age".publicKeys = [ssh];
 }
