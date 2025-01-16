8a845be87afd60affc9bd1a03a73ff0c09ecc76b
Joshua Park <joshuprk@gmail.com>
2025-01-16 16:03:51 -0500

    fix: reduce clock speed to improve fan noise

--- hosts/pc.nix
+++ hosts/pc.nix
@@ -15,10 +15,10 @@
 
   hardware.nvidia.nvidiaPersistenced = true;
   systemd.services.nvidia-underclock = {
-    description = "Limits NVIDIA GPU clocks to 1905MHz and overclocks memory by +500MHz";
+    description = "Limits NVIDIA GPU clocks to 1700MHz and overclocks memory by +500MHz";
     wantedBy = ["multi-user.target"];
     script = ''
-      /run/current-system/sw/bin/nvidia-smi --lock-gpu-clocks 0,1905
+      /run/current-system/sw/bin/nvidia-smi --lock-gpu-clocks 0,1700
       /run/current-system/sw/bin/nvidia-smi --lock-memory-clocks=0,10001
     '';
   };
