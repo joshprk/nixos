2adab8b984c2bc496ed5d1fadd4a19d1734e7dd0
Joshua Park <joshuprk@gmail.com>
2025-02-24 23:32:56 -0500

    feat: add pc config

--- hosts/pc.nix
+++ hosts/pc.nix
@@ -1,1 +1,19 @@
-{...}: {}
+{...}: {
+  settings = {
+    profiles.desktop = true;
+
+    impermanence.enable = true;
+    secureboot.enable = true;
+
+    nvidia = {
+      enable = true;
+      tuning = {
+        enable = true;
+        gpuClock = 1700;
+        memoryClock = 10001;
+      };
+    };
+  };
+
+  zramSwap.enable = true;
+}
